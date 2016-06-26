5c1219f7dbacf356b88760285b6c51c001e3b845
Jingjing <chenjingjing@hotmail.com>
2016-06-26 12:31:10 -0700

    add code

---
+++ interview/src/main/java/pattern/factory/MailSender.java
@@ -0,0 +1,8 @@
+package pattern.factory;
+
+public class MailSender implements Sender {
+	@Override
+	public void Send() {
+		System.out.println("this is mailsender!");
+	}
+}
---
+++ interview/src/main/java/pattern/factory/Sender.java
@@ -0,0 +1,5 @@
+package pattern.factory;
+
+public interface Sender {
+	public void Send();
+}

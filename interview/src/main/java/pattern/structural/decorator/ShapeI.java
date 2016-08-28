311ec4a4750749bb7e7c6d3d5171af59e5cedd7d
Jingjing <chenjingjing@hotmail.com>
2016-08-28 14:44:38 -0700

    add

--- interview/src/main/java/pattern/structural/decorator/ShapeI.java
+++ interview/src/main/java/pattern/structural/decorator/ShapeI.java
@@ -1,5 +1,8 @@
 package pattern.structural.decorator;
 
+/**
+ *  key point is construction method of concrete class has a parameter of its parent type
+ */
 public interface ShapeI {
 	void draw();
 }
